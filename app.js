2aa718791e86fa8688b27da4657f64cc6620bbbc
Marco Fruh <marco.fruh@me.com>
2015-12-21 11:04:01 +0100

    Bug fixing port

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-var express 		= require('express');
-var exprSession = require('express-session');
-var cors 				= require('cors');
-var logger 			= require('morgan');
-var http 				= require('http');
-var bodyParser 	= require('body-parser');
-var cookieParser = require('cookie-parser');
-var	passport 		= require('passport');
-var passportConfig = require(__dirname + "/server/auth/passport-config");
-var mongoose 		= require('mongoose');
-var routes 			= require('./server/routes/index');
-var users 			= require('./server/routes/users');
-var api 				= require('./server/routes/api');
+// External Dependencies
+var express 			= require('express');
+var exprSession 	= require('express-session');
+var cors 					= require('cors');
+var logger 				= require('morgan');
+var http 					= require('http');
+var bodyParser 		= require('body-parser');
+var cookieParser 	= require('cookie-parser');
+var	passport 			= require('passport');
+var mongoose 			= require('mongoose');
+var http 					= require('http');
+// Internal Dependencies
+var passConf 			= require(__dirname + "/server/auth/passport-config");
+var routes 				= require('./server/routes/index');
+var users 				= require('./server/routes/users');
+var api 					= require('./server/routes/api');
 
 var	app = express();
 
@@ -30,7 +33,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(exprSession({ secret: 'keyboard catandmouse', resave: true, saveUninitialized: true }));
 
 // Configure Passport Authentication
-passportConfig();
+passConf();
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -40,7 +43,9 @@ app.use('/users', users);
 app.use('/api', api);
 app.use(express.static(__dirname + '/client'));
 
+// CONFIGURE PORT FOR DEV AND PROD, START SERVER
+app.set('port', process.env.PORT || 3000);
 var server = http.createServer(app);
-server.listen(3000, function() {
-	console.log("Nightlife server listening on port 3000 ...");
+server.listen(app.get('port'), function() {
+	console.log('Nightlife server listening on port ' + app.get('port') + ' ...');
 });
